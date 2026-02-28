b699b61922dd0fa87d364f14d34bd05fa187d71c
LDH1401 <hungleduy278@gmail.com>
2026-02-28 15:58:19 +0700

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,38 @@ import taskRoutes from './routes/taskRoutes.js';
 import { connect } from 'mongoose';
 import { connectDB } from './config/db.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url'; // 1. Thêm thư viện này
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
-const  app = express();
+// 2. Khai báo __dirname chuẩn xác thay cho path.resolve()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// ... các phần code dưới giữ nguyên
 
 // middleware
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173"}));
+if(process.env.NODE_ENV === "development") {
+    app.use(cors({origin: "http://localhost:5173"})); // Chỉ cho phép frontend chạy trên localhost:5173 truy cập API trong môi trường phát triển
+}
 
 app.use("/api/tasks", taskRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    const frontendPath = path.join(__dirname, "..", "..", "frontend", "dist");
+
+    app.use(express.static(frontendPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
+    });
+}
 
 connectDB().then(() => { // kết nối database thành công thì mới bắt đầu server
     app.listen(PORT, () => {
